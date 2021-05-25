accc6bf9a1a41f07b70b8662f67fd24bde413106
120icq <pelmenpiksel@gmail.com>
2021-05-25 23:30:01 +0500

    0.0.1

---
+++ DataBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Npgsql.EntityFrameworkCore;
+
+namespace Practica
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime LastLogin { get; set; }
+
+
+    }
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataBaseContext() 
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=practice;Username=root;Password=1234");
+    }
+}
