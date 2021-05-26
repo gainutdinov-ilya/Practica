ea730fbcc24a28b0be86eaab357bec7a34f9e0e3
120icq <pelmenpiksel@gmail.com>
2021-05-26 19:40:06 +0500

    0.0.4

--- Register.xaml.cs
+++ Register.xaml.cs
@@ -23,12 +23,36 @@ namespace Practica
         {
             InitializeComponent();
         }
-        
+
         public void CheckForm(object sender, RoutedEventArgs e)
         {
-            if (Name.Text.Length < 2 || Name.Text.Length > 32) MessageBox.Show("Имя может иметь длинну от 3 до 32 символов");
-            else if (Surname.Text.Length < 2 || Name.Text.Length > 32) MessageBox.Show("Фамилия может иметь длинну от 3 до 32 символов");
-            
+            //Проверка правильности заполнения формы
+            if (Name.Text.Length < 2 || Name.Text.Length > 32 || Name.Text.Replace(" ", string.Empty).Length < Name.Text.Length)
+            {
+                MessageBox.Show("Имя может иметь длинну от 2 до 32 символов, пробелы недопустимы");
+                return;
+            }
+            if (Surname.Text.Length < 2 || Surname.Text.Length > 32 || Surname.Text.Replace(" ",string.Empty).Length < Surname.Text.Length) 
+            {    
+                MessageBox.Show("Фамилия может иметь длинну от 2 до 32 символов, пробелы недопустимы");
+                return;
+            }
+            if (Login.Text.Length < 6 || Login.Text.Length > 16 || Login.Text.Replace(" ", string.Empty).Length < Surname.Text.Length)
+            {
+                MessageBox.Show("Логин может иметь длинну от 6 до 16 символов, пробелы недопустимы");
+                return;
+            }
+            if (Password.Password.Length < 6 || Password.Password.Length > 32 || Password.Password.Replace(" ", string.Empty).Length < Password.Password.Length)
+            {
+                MessageBox.Show("Пароль может иметь длинну от 6 до 16 символов, пробелы недопустимы");
+                return;
+            }
+            if (Password.Password != RepeatedPassword.Password)
+            {
+                MessageBox.Show("Введёные пароли не совпадают");
+                return;
+            }
+            //
         }
     }
 }
---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+#nullable disable
+
+namespace Practica
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public BitArray Surname { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public DateTime LastLogin { get; set; }
+        public DateTime BanAuth { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
