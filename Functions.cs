2e939af66cfeafb4121a24fd6f84b2557abe6ad2
120icq <53002134+rainbow-unicorn120@users.noreply.github.com>
2021-05-27 10:23:45 +0500

    0.0.4.6.4

--- Functions.cs
+++ Functions.cs
@@ -5,7 +5,7 @@ using System.Security.Cryptography;
 //файл для хранения функций, использующихся во любых файлах программы 
 namespace Practica
 {
-    class Security { 
+    class Encryption { 
         public static string GetSHA256(string input)
         {
             return BitConverter.ToString(SHA256.Create().ComputeHash(Encoding.Default.GetBytes(input)));
